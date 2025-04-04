3d4f983686ac8187ca71f3749b85f3fd0f00c392
jenni <jenni@DESKTOP-Q4IKPSR>
2025-04-03 21:41:59 -0300

    Controlador HelloWorld/Welcome

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
